49568615e61547fc38cb7229aa971b7d597c0132
= <=>
2017-04-03 01:42:23 +0300

    Added new scripts, updated old

--- lab_6/1_ru_lost_update_T1.sql
+++ lab_6/1_ru_lost_update_T1.sql
@@ -1,5 +1,5 @@
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTEDBEGIN TRANSACTION declare @Текущий_приз moneyset @Текущий_приз =  ( SELECT Награды_спортсмен.Размер_выплаты FROM Награды_спортсмен WHERE Спортсмен_ид = 1)WAITFOR DELAY '00:00:10';UPDATE Награды_спортсмен SET Размер_выплаты = @Текущий_приз + 500 
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTEDBEGIN TRANSACTION declare @Текущий_приз moneyset @Текущий_приз =  ( SELECT Награды_спортсмен.Размер_выплаты FROM Награды_спортсмен WHERE Спортсмен_ид = 1)WAITFOR DELAY '00:00:05';UPDATE Награды_спортсмен SET Размер_выплаты = @Текущий_приз + 500 
 WHERE Спортсмен_ид = 1
-COMMITselect * from Награды_спортсмен/*UPDATE Награды_спортсмен SET Размер_выплаты = 1500 
+COMMITselect @@TRANCOUNTselect * from Награды_спортсмен/*UPDATE Награды_спортсмен SET Размер_выплаты = 1500 
 WHERE Спортсмен_ид = 1
 */
--- lab_6/1_ru_lost_update_T2.sql
+++ lab_6/1_ru_lost_update_T2.sql
@@ -1,4 +1,4 @@
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTEDBEGIN TRANSACTION declare @Текущий_приз moneyset @Текущий_приз =  ( SELECT Награды_спортсмен.Размер_выплаты FROM Награды_спортсмен WHERE Спортсмен_ид = 1)UPDATE Награды_спортсмен SET Размер_выплаты = @Текущий_приз + 200 
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTEDBEGIN TRANSACTION declare @Текущий_приз moneyset @Текущий_приз =  ( SELECT Награды_спортсмен.Размер_выплаты FROM Награды_спортсмен WHERE Спортсмен_ид = 1)UPDATE Награды_спортсмен SET Размер_выплаты = @Текущий_приз + 200 
 WHERE Спортсмен_ид = 1
 COMMIT/*select * from Награды_спортсменUPDATE Награды_спортсмен SET Размер_выплаты = 1500 
 WHERE Спортсмен_ид = 1
