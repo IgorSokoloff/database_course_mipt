33e3b89f066bba0039ae88367a436df9fec2298c
IgorSokoloff <sokol_hp-1996@mail.ru>
2017-06-01 00:46:58 +0300

    Changed encoding

--- lab_6(transactions)/1_ru_lost_update_ver2_T1.sql
+++ lab_6(transactions)/1_ru_lost_update_ver2_T1.sql
@@ -1,4 +1,4 @@
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTEDBEGIN TRANSACTION declare @Текущий_приз moneyset @Текущий_приз =  ( SELECT Награды_спортсмен.Размер_выплаты FROM Награды_спортсмен WHERE Спортсмен_ид = 1)WAITFOR DELAY '00:00:05';UPDATE Награды_спортсмен SET Размер_выплаты = Размер_выплаты + 500 
+﻿SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTEDBEGIN TRANSACTION declare @Текущий_приз moneyset @Текущий_приз =  ( SELECT Награды_спортсмен.Размер_выплаты FROM Награды_спортсмен WHERE Спортсмен_ид = 1)WAITFOR DELAY '00:00:05';UPDATE Награды_спортсмен SET Размер_выплаты = Размер_выплаты + 500 
 WHERE Спортсмен_ид = 1
 COMMITSELECT Награды_спортсмен.Размер_выплаты FROM Награды_спортсмен WHERE Спортсмен_ид = 1--select @@TRANCOUNT--select * from Награды_спортсмен/*UPDATE Награды_спортсмен SET Размер_выплаты = 1500 
 WHERE Спортсмен_ид = 1
